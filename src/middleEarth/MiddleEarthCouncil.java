3df82d4df5cffbe155dbbf3204de70c9c20cf7fb
thejnar <157063962+thejnar@users.noreply.github.com>
2025-03-04 15:16:47 -0600

    part 2/3
    part 2/3

---
+++ src/middleEarth/MiddleEarthCouncil.java
@@ -0,0 +1,35 @@
+package middleEarth;
+
+/**
+ * Singleton class for managing Middle-earth characters.
+ */
+public class MiddleEarthCouncil {
+    private static MiddleEarthCouncil instance;
+    private CharacterManager characterManager;
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private MiddleEarthCouncil() {
+        characterManager = new CharacterManager();
+    }
+
+    /**
+     * Retrieves the single instance of MiddleEarthCouncil.
+     * @return the singleton instance of MiddleEarthCouncil.
+     */
+    public static MiddleEarthCouncil getInstance() {
+        if (instance == null) {
+            instance = new MiddleEarthCouncil();
+        }
+        return instance;
+    }
+
+    /**
+     * Provides access to the CharacterManager.
+     * @return the CharacterManager instance.
+     */
+    public CharacterManager getCharacterManager() {
+        return characterManager;
+    }
+}
